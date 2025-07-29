f46507a43aabd22c625150fa6a1c70202b32bc93
Slav Slavov <slavslavov10@yahoo.com>
2025-07-29 16:39:56 +0300

    update pagination settings for Question API; increase default page size to 10

--- api/pagination.py
+++ api/pagination.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 
 
 class QuestionApiPagination(PageNumberPagination):
-	page_size = 3
+	page_size = 10
 	page_size_query_param = 'page_size'
 	max_page_size = 50
 
