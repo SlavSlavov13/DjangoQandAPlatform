04bac407996a0b585cf0ad6b30c028a0b92e665b
Slav Slavov <slavslavov10@yahoo.com>
2025-08-03 14:47:35 +0300

    enhance password reset functionality; add email subject and templates for password reset notifications

--- DjangoQandAPlatform/urls.py
+++ DjangoQandAPlatform/urls.py
@@ -44,20 +44,35 @@ urlpatterns = [
 
     # Password reset urls
     path('password-reset/',
-         auth_views.PasswordResetView.as_view(template_name='password/password_reset.html'),
-         name='password_reset'),
+         auth_views.PasswordResetView.as_view(
+             template_name='password/password_reset.html',
+             subject_template_name='password/password_reset_subject.txt',
+             email_template_name='password/password_reset_email.txt',
+             html_email_template_name='password/password_reset_email.html',
+         ),
+         name='password_reset'
+         ),
 
     path('password-reset/done/',
-         auth_views.PasswordResetDoneView.as_view(template_name='password/password_reset_done.html'),
-         name='password_reset_done'),
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='password/password_reset_done.html'
+         ),
+         name='password_reset_done'
+         ),
 
     path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='password/password_reset_confirm.html'),
-         name='password_reset_confirm'),
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='password/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'
+         ),
 
     path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='password/password_reset_complete.html'),
-         name='password_reset_complete'),
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='password/password_reset_complete.html'
+         ),
+         name='password_reset_complete'
+         ),
     path('password-reset-auto/', auto_password_reset_request, name='password_reset_auto'),
 ]
 
